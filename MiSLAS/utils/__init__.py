dc5f43275ed08c41f2f6eab47b5448f9b28b3f90
jiequan <jiequancui@gmail.com>
2023-05-22 22:20:33 +0800

    update

--- MiSLAS/utils/__init__.py
+++
@@ -1,9 +0,0 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-from .logger import _C as config
-from .logger import update_config, create_logger
-
-from .metric import accuracy, calibration
-from .meter import AverageMeter, ProgressMeter
--- PaCo/LT/models/__init__.py
+++
@@ -1,2 +0,0 @@
-from .resnet_cifar import *
-from .resnet_imagenet import *
